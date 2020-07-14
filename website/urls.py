42adb3518e03b5ea3d72fd1a6bc523c3af7e6596
Matheus Elyasha <matheuselyasha@gmail.com>
2020-07-14 16:38:15 +0200

    Added basic urls to the project

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.index, name='index'),
+    path('login/', views.LoginView.login, name='login'),
+    path('logout/', views.LogoutView.logout, name='logout'),
+    path('register/', views.RegisterView.register, name='register'),
+    path('indiqueeganhe/', views.ReferAndWinView.as_view, name='referandwin'),
+    path('comofunciona', views.HowItWorksView.as_view, name='howitworks'),
+    path('quemsomos', views.WhoWeAreView.as_view, name='whoweare'),
+    path('contato', views.ContactView.as_view, name='contact')
 
 ]
