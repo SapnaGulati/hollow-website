4558aefeef430e7a92e29cc1f28d67cdbc56e2b5
Matheus Elyasha <matheuselyasha@gmail.com>
2020-07-14 15:48:51 +0200

    Added phonenumber_field to INSTALLED_APPS in settings

--- hollow/settings.py
+++ hollow/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-pt'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Lisbon'
 
 USE_I18N = True
 
