afb4e51d6cea5e731cbd3effb1529d513e77f0ac
gabrielmaxgb <gmaxgomes@gmail.com>
2020-07-22 19:18:45 -0300

    reformat code of all the projects and resolved typo of closed div

--- website/static/website/js/custom.js
+++ website/static/website/js/custom.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, options);
 });
 
 // Or with jQuery
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.fixed-action-btn').floatingActionButton();
     $('.fixed-action-btn.top').floatingActionButton({
         direction: 'top'
