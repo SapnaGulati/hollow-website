c8a545b23ae5a9aa1c667d7f96e2fb19ffdcb11f
Filipe Machado <filipe__machado@hotmail.com>
2020-07-28 19:46:59 -0300

    Add course sub-structures

--- website/models/course.py
+++ website/models/course.py
@@ -1,7 +1,31 @@
 from django.db import models
 
+
 class Course(models.Model):
-    title = models.TextField()
+    title = models.CharField()
+    macrotopic = models.ForeignKey(Macrotopic, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+
+class Macrotopic(models.Model):
+    title = models.CharField()
+    microtopic = models.ForeignKey(microtopic, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+
+class Microtopic(models.Model):
+    title = models.CharField()
+    video = models.ForeignKey(VideoLesson, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class VideoLesson(models.Model):
+    title = models.CharField()
+    def __str__(self):
+        return self.title
+         
