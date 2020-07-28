855efb17caba1597c7380957e4be4ccda20bdb2d
Filipe Machado <filipe__machado@hotmail.com>
2020-07-28 20:49:39 -0300

    Improve course related models

--- website/models/__init__.py
+++ website/models/__init__.py
@@ -1,2 +1,2 @@
 from website.models.user import User
-from website.models.course import Course
+from website.models.course import Course, Macrotopic, Microtopic, VideoLesson, ExerciseList
--- website/models/course.py
+++ website/models/course.py
@@ -1,31 +1,47 @@
+import datetime
+
 from django.db import models
 
 
 class Course(models.Model):
-    title = models.CharField()
-    macrotopic = models.ForeignKey(Macrotopic, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    macrotopic = models.ForeignKey('Macrotopic', on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
 
 class Macrotopic(models.Model):
-    title = models.CharField()
-    microtopic = models.ForeignKey(microtopic, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-
-
-class Microtopic(models.Model):
-    title = models.CharField()
-    video = models.ForeignKey(VideoLesson, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    microtopic = models.ForeignKey('Microtopic', on_delete=models.CASCADE)
+    exercise_list = models.ForeignKey('ExerciseList', verbose_name='exercise list', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
 
 
+class Microtopic(models.Model):
+    title = models.CharField(max_length=200)
+    video = models.ForeignKey('VideoLesson', on_delete=models.CASCADE)
+    importance = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
 class VideoLesson(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
+    url = models.URLField(max_length=200)
+    duration = models.DurationField(default=datetime.timedelta(minutes=0))
+
     def __str__(self):
         return self.title
          
+
+class ExerciseList(models.Model):
+    title = models.CharField(default='Exercícios e exemplos de aplicação.', max_length=200)
+    url = models.URLField(max_length=200)
+    
+    def __str__(self):
+        return self.title
+    
