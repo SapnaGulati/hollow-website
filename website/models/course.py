bcd24c2e73b29a94a7aaca1cdbd4669d081b31c7
Filipe Machado <filipe__machado@hotmail.com>
2020-07-30 22:56:28 -0300

    Add comments

--- website/models/course.py
+++ website/models/course.py
@@ -4,70 +4,82 @@ from django.db import models
 
 
 class Course(models.Model):
+    # DATABASE FIELDS
     title = models.CharField(verbose_name='Título' ,max_length=200)
     macrotopic = models.ManyToManyField('Macrotopic', verbose_name='Macrotópico')
-
-    def __str__(self):
-        return self.title
-
-
+    
+    # META CLASS
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
 
+    # TO STRING METHOD
+    def __str__(self):
+        return self.title
+
 
 class Macrotopic(models.Model):
+    # DATABASE FIELDS
     title = models.CharField(verbose_name='Título', max_length=200)
     microtopic = models.ManyToManyField('Microtopic', verbose_name='Microtópico')
     exercise_list = models.ForeignKey('ExerciseList', verbose_name='Lista de Exercícios', on_delete=models.CASCADE)
     
-    def __str__(self):
-        return self.title
-
-
+    # META CLASS
     class Meta:
         verbose_name = 'Macrotópico'
         verbose_name_plural = 'Macrotópicos'
 
+    # TO STRING METHOD
+    def __str__(self):
+        return self.title
+
 
 class Microtopic(models.Model):
+    # DATABASE FIELDS
     title = models.CharField(verbose_name='Título', max_length=200)
     video = models.ForeignKey('VideoLesson', verbose_name='Vídeoaula', on_delete=models.CASCADE)
     importance = models.SmallIntegerField(verbose_name='Importância')
 
-    def __str__(self):
-        return self.title
 
-
+    # META CLASS
     class Meta:
         verbose_name = 'Microtópico'
         verbose_name_plural = 'Microtópicos'
 
+    # TO STRING METHOD
+    def __str__(self):
+        return self.title
+    
 
 class VideoLesson(models.Model):
+    # DATABASE FIELDS
     title = models.CharField(verbose_name='Título', max_length=200)
     url = models.URLField(max_length=200)
     duration = models.DurationField(verbose_name='Duração', default=datetime.timedelta(minutes=0))
 
-    def __str__(self):
-        return self.title
 
-
+    # META CLASS
     class Meta:
         verbose_name = 'Vídeoaula'
         verbose_name_plural = 'Vídeoaulas'
-    
+
+    # TO STRING METHOD
+    def __str__(self):
+        return self.title    
+
 
 class ExerciseList(models.Model):
+    # DATABASE FIELDS
     title = models.CharField(verbose_name='Título', default='Exercícios e exemplos de aplicação.', max_length=200)
     url = models.URLField(max_length=200)
     
-    def __str__(self):
-        return self.title
-    
-    
+    # META CLASS
     class Meta:
         verbose_name = 'Lista de Exercícios'
         verbose_name_plural = 'Listas de Exercícios'
+    
+    # TO STRING METHOD
+    def __str__(self):
+        return self.title
     
     
