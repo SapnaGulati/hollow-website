4ffe056560afc7ba17397394249eaeaa3572d0ef
Filipe Machado <filipe__machado@hotmail.com>
2020-07-30 21:33:34 -0300

    Add verbose name to objects

--- website/models/course.py
+++ website/models/course.py
@@ -4,20 +4,28 @@ from django.db import models
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(verbose_name='Título' ,max_length=200)
     macrotopic = models.ForeignKey('Macrotopic', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
 
 class Macrotopic(models.Model):
     title = models.CharField(max_length=200)
     microtopic = models.ForeignKey('Microtopic', on_delete=models.CASCADE)
-    exercise_list = models.ForeignKey('ExerciseList', verbose_name='exercise list', on_delete=models.CASCADE)
+    exercise_list = models.ForeignKey('ExerciseList', verbose_name='lista de exercícios', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Macrotópico'
+        verbose_name_plural = 'Macrotópicos'
 
 
 class Microtopic(models.Model):
@@ -28,6 +36,10 @@ class Microtopic(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Microtópico'
+        verbose_name_plural = 'Microtópicos'
+
 
 class VideoLesson(models.Model):
     title = models.CharField(max_length=200)
@@ -37,6 +49,10 @@ class VideoLesson(models.Model):
     def __str__(self):
         return self.title
          
+    class Meta:
+        verbose_name = 'Vídeoaula'
+        verbose_name_plural = 'Vídeoaulas'
+    
 
 class ExerciseList(models.Model):
     title = models.CharField(default='Exercícios e exemplos de aplicação.', max_length=200)
@@ -44,4 +60,9 @@ class ExerciseList(models.Model):
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        verbose_name = 'Lista de Exercícios'
+        verbose_name_plural = 'Listas de Exercícios'
+    
     
