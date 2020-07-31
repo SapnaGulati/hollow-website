f3dd3b0ea90d50249268763ad9250d74f506b9f7
Filipe Machado <filipe__machado@hotmail.com>
2020-07-30 22:22:18 -0300

    Add more 'verbose_name' fields

--- website/models/course.py
+++ website/models/course.py
@@ -5,10 +5,11 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(verbose_name='Título' ,max_length=200)
-    macrotopic = models.ManyToManyField('Macrotopic')
+    macrotopic = models.ManyToManyField('Macrotopic', verbose_name='Macrotópico')
 
     def __str__(self):
         return self.title
+
 
     class Meta:
         verbose_name = 'Curso'
@@ -16,12 +17,13 @@ class Course(models.Model):
 
 
 class Macrotopic(models.Model):
-    title = models.CharField(max_length=200)
-    microtopic = models.ManyToManyField('Microtopic')
-    exercise_list = models.ForeignKey('ExerciseList', verbose_name='lista de exercícios', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Título', max_length=200)
+    microtopic = models.ManyToManyField('Microtopic', verbose_name='Microtópico')
+    exercise_list = models.ForeignKey('ExerciseList', verbose_name='Lista de Exercícios', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
+
 
     class Meta:
         verbose_name = 'Macrotópico'
@@ -29,12 +31,13 @@ class Macrotopic(models.Model):
 
 
 class Microtopic(models.Model):
-    title = models.CharField(max_length=200)
-    video = models.ForeignKey('VideoLesson', on_delete=models.CASCADE)
-    importance = models.SmallIntegerField()
+    title = models.CharField(verbose_name='Título', max_length=200)
+    video = models.ForeignKey('VideoLesson', verbose_name='Vídeoaula', on_delete=models.CASCADE)
+    importance = models.SmallIntegerField(verbose_name='Importância')
 
     def __str__(self):
         return self.title
+
 
     class Meta:
         verbose_name = 'Microtópico'
@@ -42,24 +45,26 @@ class Microtopic(models.Model):
 
 
 class VideoLesson(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(verbose_name='Título', max_length=200)
     url = models.URLField(max_length=200)
-    duration = models.DurationField(default=datetime.timedelta(minutes=0))
+    duration = models.DurationField(verbose_name='Duração', default=datetime.timedelta(minutes=0))
 
     def __str__(self):
         return self.title
-         
+
+
     class Meta:
         verbose_name = 'Vídeoaula'
         verbose_name_plural = 'Vídeoaulas'
     
 
 class ExerciseList(models.Model):
-    title = models.CharField(default='Exercícios e exemplos de aplicação.', max_length=200)
+    title = models.CharField(verbose_name='Título', default='Exercícios e exemplos de aplicação.', max_length=200)
     url = models.URLField(max_length=200)
     
     def __str__(self):
         return self.title
+    
     
     class Meta:
         verbose_name = 'Lista de Exercícios'
