1c76528c0d3f1f9f79f0ba92c994c96bbff75266
Matheus Elyasha <matheuselyasha@gmail.com>
2020-08-04 22:47:59 +0200

    Added development environment to project

---
+++ hollow/settings/development.py
@@ -0,0 +1,10 @@
+from hollow.settings.base import *
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
