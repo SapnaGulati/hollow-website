eb8e532bdfd4739f9dc3fba7e66521a0749e2eae
Matheus Elyasha <matheuselyasha@gmail.com>
2020-08-05 21:50:50 +0200

    Added change information view and change password view

--- website/views/user.py
+++ website/views/user.py
@@ -24,3 +24,25 @@ class UserView(generic.ListView):
             return render(request, 'website/user_profile.html', {'user': user})
         except User.DoesNotExist:
             return render(request, 'website/user_does_not_exist.html')
+
+
+class ChangeInformationView(generic.ListView):
+    template_name = 'website/change_information.html'
+
+    def change_information(request):
+        """"
+        Temporary solution while we do not construct the queryset method
+        """
+
+        return render(request, 'website/change_information.html')
+
+
+class ChangePasswordView(generic.ListView):
+    template_name = 'website/change_password.html'
+
+    def changed_password(request):
+        """"
+        Temporary solution while we do not construct the queryset method
+        """
+
+        return render(request, 'website/change_password.html')
