35a8a1ed4d5251729e8c5b8d32a251e21c57bd55
Filipe Machado <filipe__machado@hotmail.com>
2020-08-08 01:24:01 -0300

    Make the page dinamic to the course

--- website/views/courses.py
+++ website/views/courses.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 
+from website.models import Course
+
 
 class CoursesView(generic.ListView):
-    def as_view(request):
-        return render(request, 'website/course_page.html')
+    def as_view(request, course_id):
+        course = Course.objects.get(id=course_id)
+        context = {
+            "course": course
+        }
+        return render(request, 'website/course_page.html', context)
